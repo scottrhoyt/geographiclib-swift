7189e8de99f17f4a0c64923e96ab8451fa3d2a29
Scott Hoyt <scottrhoyt@gmail.com>
2025-06-01 20:24:41 -0700

    Stub out Geod and GeodTests.

---
+++ Sources/GeographicLib/Geod.swift
@@ -0,0 +1,13 @@
+//
+//  Geod.swift
+//  GeographicLib
+//
+//  Created by Scott Hoyt on 6/1/25.
+//
+
+import Foundation
+import CGeographicLib
+
+public struct Geod {
+    /* To fill out */
+}
---
+++ Tests/GeographicLibTests/GeodTests.swift
@@ -0,0 +1,14 @@
+//
+//  GeodTests.swift
+//  GeographicLib
+//
+//  Created by Scott Hoyt on 6/1/25.
+//
+
+import Foundation
+@testable import GeographicLib
+import Testing
+
+struct GeodTests {
+    /* To fill out */
+}
